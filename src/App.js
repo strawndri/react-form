66d2b29d3dafa20072a872d56c0f8ad35f387fe1
andrieli <andrieliluci@gmail.com>
2022-05-08 19:36:23 -0300

    create Main and add styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Main from "./components/Main";
+
 function App() {
   return (
-    <div className="App">
-      Hey
-    </div>
+    <Main>
+      {/* <Form>
+        <Field.text />
+      </Form> */}
+    </Main>
   );
 }
 
---
+++ src/components/Main/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Main = styled.main`
+
+    width: 100vw;
+    height: 100vh;
+
+    background-color: #081229;
+`
+
+export default Main;
