6aae95bf1add47768bdb5c612e8d6f7e358f9b06
andrieli <andrieliluci@gmail.com>
2022-05-14 21:16:09 -0300

    create Base.js -> global style

---
+++ src/styles/elements/Base.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const Base = createGlobalStyle`
+    body {
+        font-family: 'Comfortaa', cursive;
+        color: #f2f2f2;
+    }
+`
+
+export default Base;
